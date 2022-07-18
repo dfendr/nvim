d4b87d7daec676e797708ad234b36327bea6f077
postfen <26173007+postfen@users.noreply.github.com>
2022-07-18 00:57:29 -0600

    lualiine (Doesn't seem to work)

---
+++ lua/user/lualine.lua
@@ -0,0 +1,38 @@
+local status_ok, configs = pcall(require, "lualine.setup")
+if not status_ok then
+    return
+end
+
+
+local custom_gruvbox = require'lualine.themes.gruvbox'
+
+configs.setup
+    {
+    options = { theme = 'gruvbox',
+    icons_enabled = true,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    globalstatus = false,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+  }
+
