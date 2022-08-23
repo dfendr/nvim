7c252f34afb8fd01af5b8f2c9dba5f9be606aaff
postfen <26173007+postfen@users.noreply.github.com>
2022-08-23 11:10:47 -0600

    changed contrast for theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,42 +2,43 @@
 
 
 -- Default Palette
-dark0_hard = "#1d2021"
-dark0 = "#282828"
-dark0_soft = "#32302f"
-dark1 = "#3c3836"
-dark2 = "#504945"
-dark3 = "#665c54"
-dark4 = "#7c6f64"
-light0_hard = "#f9f5d7"
-light0 = "#fbf1c7"
-light0_soft = "#f2e5bc"
-light1 = "#ebdbb2"
-light2 = "#d5c4a1"
-light3 = "#bdae93"
-light4 = "#a89984"
-bright_red = "#fb4934"
-bright_green = "#b8bb26"
-bright_yellow = "#fabd2f"
-bright_blue = "#83a598"
-bright_purple = "#d3869b"
-bright_aqua = "#8ec07c"
-bright_orange = "#fe8019"
-neutral_red = "#cc241d"
-neutral_green = "#98971a"
-neutral_yellow = "#d79921"
-neutral_blue = "#458588"
-neutral_purple = "#b16286"
-neutral_aqua = "#689d6a"
-neutral_orange = "#d65d0e"
-faded_red = "#9d0006"
-faded_green = "#79740e"
-faded_yellow = "#b57614"
-faded_blue = "#076678"
-faded_purple = "#8f3f71"
-faded_aqua = "#427b58"
-faded_orange = "#af3a03"
-gray = "#928374"
+local dark0_hard = "#1d2021"
+local dark0 = "#282828"
+local dark0_soft = "#32302f"
+local dark1 = "#3c3836"
+local dark2 = "#504945"
+local dark3 = "#665c54"
+local dark4 = "#7c6f64"
+local light0_hard = "#f9f5d7"
+local light0 = "#fbf1c7"
+local light0_soft = "#f2e5bc"
+local light1 = "#ebdbb2"
+local light2 = "#d5c4a1"
+local light3 = "#bdae93"
+local light4 = "#a89984"
+local bright_red = "#fb4934"
+local bright_green = "#b8bb26"
+local bright_yellow = "#fabd2f"
+local bright_blue = "#83a598"
+local bright_purple = "#d3869b"
+local bright_aqua = "#8ec07c"
+local bright_orange = "#fe8019"
+local neutral_red = "#cc241d"
+local neutral_green = "#98971a"
+local neutral_yellow = "#d79921"
+local neutral_blue = "#458588"
+local neutral_purple = "#b16286"
+local neutral_aqua = "#689d6a"
+local neutral_orange = "#d65d0e"
+local faded_red = "#9d0006"
+local faded_green = "#79740e"
+local faded_yellow = "#b57614"
+local faded_blue = "#076678"
+local faded_purple = "#8f3f71"
+local faded_aqua = "#427b58"
+local faded_orange = "#af3a03"
+local gray = "#928374"
+
 
 require("gruvbox").setup({
   undercurl = true,
@@ -50,7 +51,7 @@ require("gruvbox").setup({
   invert_tabline = false,
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
+  contrast = "hard", -- can be "hard", "soft" or empty string
   overrides = {
         --General
         TSOperator = {fg = bright_aqua},
