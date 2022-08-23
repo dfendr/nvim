fbddb9ad38000aea7cae3da120d01b7629f5905c
postfen <26173007+postfen@users.noreply.github.com>
2022-08-22 20:22:57 -0600

    start that LSP!!

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require "user.options"
 require "user.utils"
 require "user.keymaps"
 require "user.plugins"
---require "user.lsp"
+require "user.lsp"
 require "user.colorscheme"
 require "user.nvim-tree"
 require "user.treesitter"
@@ -11,7 +11,9 @@ require "user.lualine"
 require "user.todo-comments"
 require "user.comment"
 require "user.neoscroll"
+require "user.functions"
 require "user.which-key"
+require'lspconfig'.pyright.setup{}
 -- TODO: 1. Get LSP working, 2. Get null-ls formatting working
 -- TODO: 3. DON'T GET DISTRACTED BY ANYTHING UNTIL YOU COMPLETE 1 AND 2
 
