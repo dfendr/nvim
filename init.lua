729a409038579f4b9cd0cdc3b8244a803d6a5c0b
postfen <26173007+postfen@users.noreply.github.com>
2022-08-25 23:18:20 -0600

    Updated Todo

--- init.lua
+++ init.lua
@@ -1,32 +1,36 @@
-require "user.impatient"
-require "user.options"
-require "user.keymaps"
-require "user.utils"
-require "user.plugins"
-require "user.colorscheme"
-require "user.nvim-tree"
-require "user.alpha"
-require "user.notify"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.ts-context"
-require "user.cmp"
-require "user.lualine"
-require "user.gitsigns"
-require "user.toggleterm"
-require "user.illuminate"
+require("user.impatient")
+require("user.options")
+require("user.keymaps")
+require("user.utils")
+require("user.plugins")
+require("user.colorscheme")
+require("user.nvim-tree")
+require("user.alpha")
+require("user.notify")
+require("user.lsp")
+require("user.telescope")
+require("user.treesitter")
+require("user.ts-context")
+require("user.cmp")
+require("user.lualine")
+require("user.gitsigns")
+require("user.toggleterm")
+require("user.illuminate")
 --require "user.winbar" needs NAVIC highlight groups
-require "user.indentblankline"
-require "user.todo-comments"
-require "user.comment"
-require "user.autopairs"
-require "user.neoscroll"
-require "user.renamer"
-require "user.functions"
-require "user.jaq"
+require("user.indentblankline")
+require("user.todo-comments")
+require("user.comment")
+require("user.autopairs")
+require("user.neoscroll")
+require("user.renamer")
+require("user.functions")
+require("user.jaq")
 --require "user.sniprun"
 -- require "user.shade"
-require "user.colorizer"
-require "user.which-key"
--- TODO: 1. Get CodeRunner set up working (toggle term?)
+require("user.colorizer")
+require("user.which-key")
+-- TODO: 1. Get Debugger working (Python, Rust, C, Bash (MAYBE JAVA?))
+-- TODO:  2. Smooth out any bugs, get UI tightened. General tweaking. 
+-- TODO: 3.  Check out how long it takes for neovim to start. 
+-- TODO:  4. I think that's it. I think we're done. Celebrate. Use neovim <3 
+
