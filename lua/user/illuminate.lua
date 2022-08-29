703a08348143775a9b8626dc370d05d62d26b6dd
postfen <26173007+postfen@users.noreply.github.com>
2022-08-28 22:06:30 -0600

    merged PR to gruvbox-baby that adds Illuminate

--- lua/user/illuminate.lua
+++ lua/user/illuminate.lua
@@ -29,7 +29,3 @@ illuminate.configure({
     },
     under_cursor = true;
 })
-
-vim.cmd [[highlight! link IlluminatedWordText CursorLine]]
-vim.cmd [[highlight! link IlluminatedWordRead CursorLine]]
-vim.cmd [[highlight! link IlluminatedWordWrite CursorLine]]
