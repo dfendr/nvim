a48742608c951afc9ce937646decfd860f327b84
postfen <26173007+postfen@users.noreply.github.com>
2022-10-04 14:59:40 -0600

    removed text

--- lua/user/image.lua
+++ lua/user/image.lua
@@ -3,13 +3,6 @@ if not status_ok then
     return
 end
 
--- % / $file • Current File
--- # / $altFile • Alternate File
--- $dir • Current Working Directory
--- $filePath • Path to Current File
--- $fileBase • Basename of File (no extension)
--- $moduleName • Python Module Name
-
 image.setup({ -- Require and call setup function somewhere in your init.lua
     render = {
         min_padding = 5,
