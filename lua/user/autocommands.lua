b4a7150b501c811d7da563f9ae107c407deaa001
postfen <26173007+postfen@users.noreply.github.com>
2022-10-06 23:48:23 -0600

    commented out for now

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,10 +1,10 @@
 -- Turn off IndentBlankline for big files
-vim.api.nvim_create_autocmd("BufEnter", {
-    group = vim.api.nvim_create_augroup("IndentBlanklineBigFile", {}),
-    pattern = "*",
-    callback = function()
-        if vim.api.nvim_buf_line_count(0) > 1000 then
-            require("indent_blankline.commands").disable()
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--     group = vim.api.nvim_create_augroup("IndentBlanklineBigFile", {}),
+--     pattern = "*",
+--     callback = function()
+--         if vim.api.nvim_buf_line_count(0) > 1000 then
+--             require("indent_blankline.commands").disable()
+--         end
+--     end,
+-- })
