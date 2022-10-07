db9155daa0aca37187d688eee3a26acf50317c1d
postfen <26173007+postfen@users.noreply.github.com>
2022-10-07 10:38:32 -0600

    Changing context

--- lua/user/indentblankline.lua
+++ lua/user/indentblankline.lua
@@ -22,7 +22,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 indentblankline.setup {
     -- for example, context is off by default, use this to turn it on
-    show_current_context = false,
+    show_current_context = true,
     show_current_context_start = true,
     space_char_blankline = " ",
     filetype_exclude = {
