ef0d2c0bfd95f78e39000d55356115aca8867c03
postfen <26173007+postfen@users.noreply.github.com>
2022-10-06 23:47:49 -0600

    ignoring events did the trick, no lag.

--- ftplugin/text.lua
+++ ftplugin/text.lua
@@ -1,5 +1,6 @@
 vim.cmd("let loaded_matchparen = 1")
 vim.cmd("set noloadplugins") -- Still doesn't match --noplugin behaviour...
+vim.cmd("set eventignore=all") -- Still doesn't match --noplugin behaviour...
 local options = {}
 
 for k, v in pairs(options) do
