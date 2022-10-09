9a9f134fc7c4b5d8b07fc2cafc33bd1765a47fcc
postfen <26173007+postfen@users.noreply.github.com>
2022-10-09 00:16:18 -0600

    Added lazygit toggle

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -62,6 +62,29 @@ function _GITUI_TOGGLE()
   gitui:toggle()
 end
 
+local lazygit= Terminal:new {
+  cmd = "lazygit",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "none",
+    width = 100000,
+    height = 100000,
+  },
+  on_open = function(_)
+    vim.cmd "startinsert!"
+    -- vim.cmd "set laststatus=0"
+  end,
+  on_close = function(_)
+    -- vim.cmd "set laststatus=3"
+  end,
+  count = 99,
+}
+
+function _LAZYGIT_TOGGLE()
+  lazygit:toggle()
+end
+
 local node = Terminal:new { cmd = "node", hidden = true }
 
 function _NODE_TOGGLE()
