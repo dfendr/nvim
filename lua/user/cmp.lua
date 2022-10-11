7f050119711d60a6643cdfc9b701f09040416b9b
postfen <26173007+postfen@users.noreply.github.com>
2022-10-11 14:27:50 -0600

    changed keybind back for cmp, caused delays when using esc

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -85,7 +85,7 @@ cmp.setup({
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
         ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<esc>"] = cmp.mapping({
+        ["<C-e>"] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         }),
