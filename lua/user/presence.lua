991a3452de2e7247b061e5c04bfc8ebc03e48515
postfen <26173007+postfen@users.noreply.github.com>
2022-10-27 19:58:30 -0600

    minor changes, perl/awk run support

--- lua/user/presence.lua
+++ lua/user/presence.lua
@@ -3,19 +3,19 @@ local status_ok, presence = pcall(require, "presence")
 if not status_ok then
     return
 end
-
+-- 🌲
 presence:setup({
     -- General options
     auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
-    neovim_image_text = "🍄Fenvim", -- Text displayed when hovered over the Neovim image
+    neovim_image_text = "🍄fenvim", -- Text displayed when hovered over the Neovim image
     -- main_image = "neovim", -- Main image display (either "neovim" or "file")
     main_image = "file", -- Main image display (either "neovim" or "file")
     client_id = "***REMOVED***", -- Use your own Discord application client id (not recommended)
     log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
     debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-    enable_line_number = true, -- Displays the current line number instead of the current project
+    enable_line_number = false, -- Displays the current line number instead of the current project
     blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-    buttons = true, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+    buttons = false, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
     file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
 
     -- Rich Presence text options
