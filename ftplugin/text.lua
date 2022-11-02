a5194a3f6303ee21dc5394cc23a3c412590274e8
postfen <26173007+postfen@users.noreply.github.com>
2022-11-01 18:15:34 -0600

    working on autocmds for text files

--- ftplugin/text.lua
+++ ftplugin/text.lua
@@ -1,7 +1,16 @@
 vim.cmd("let loaded_matchparen = 1")
 -- vim.cmd("set noloadplugins") -- Still doesn't match --noplugin behaviour...
 --TODO: Set this to only turn on on large files.
-vim.cmd("set eventignore=all") -- Still doesn't match --noplugin behaviour...
+
+--TODO: Turn this into an autocmd that turns on when entering txt,
+--and turns off when leaving
+vim.cmd("set eventignore=all -buffer")
+
+-- vim.cmd([[
+-- :autocmd WinEnter <buffer> set eventignore=all
+-- :autocmd WinLeave <buffer> set eventignore=\"\"
+-- ]])
+
 local options = {}
 
 for k, v in pairs(options) do
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -10,7 +10,7 @@ configs.setup({
     ignore_install = { "" }, -- List of parsers to ignore installing
     highlight = {
         enable = true, -- false will disable the whole extension
-        disable = { "" }, -- list of language that will be disabled
+        disable = { "text", "txt" }, -- list of language that will be disabled
         additional_vim_regex_highlighting = true,
     },
     rainbow = {
