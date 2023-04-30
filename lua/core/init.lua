e0c60a3fa7f8f6072e678e43cef4c81ed7767501
D. Fender <26173007+postfen@users.noreply.github.com>
2023-04-30 11:27:54 -0600

    refactored autocmds

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,5 @@
 require("core.options")
 require("core.keymaps")
+require("core.autocommands")
 -- On startup, until the plugin colorscheme is loaded, this colorscheme is used.
 vim.cmd([[colo retrobox]])
