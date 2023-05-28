5dd9ec0c0fe178dc83c4214a3d71768d25594422
D. Fender <26173007+postfen@users.noreply.github.com>
2023-05-28 14:47:43 -0600

    Improvements to NvimTree behaviour (sorting, sessions)
    nvim-tree migration to on_attach, sorting now puts dirs on top

--- lua/fenvim/nvim-tree.lua
+++ lua/fenvim/nvim-tree.lua
@@ -1,14 +1,93 @@
 local M = {
     "nvim-tree/nvim-tree.lua",
-    event = "VeryLazy",
     dependencies = {
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     },
     enabled = true,
-    commit = "9c97e6449b0b0269bd44e1fd4857184dfa57bb4c",
+    cmd = "NvimTreeToggle",
 }
 
--- TODO: Migrate to on_attach
+--
+local function on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- Default mappings. Feel free to modify or remove as you wish.
+    --
+    -- BEGIN_DEFAULT_ON_ATTACH
+    vim.keymap.set("n", "<C-]>", api.tree.change_root_to_node, opts("CD"))
+    vim.keymap.set("n", "<C-e>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
+    vim.keymap.set("n", "<C-k>", api.node.show_info_popup, opts("Info"))
+    vim.keymap.set("n", "<C-r>", api.fs.rename_sub, opts("Rename: Omit Filename"))
+    vim.keymap.set("n", "<C-t>", api.node.open.tab, opts("Open: New Tab"))
+    vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
+    vim.keymap.set("n", "<C-x>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+    vim.keymap.set("n", "<BS>", api.node.navigate.parent_close, opts("Close Directory"))
+    vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "<Tab>", api.node.open.preview, opts("Open Preview"))
+    vim.keymap.set("n", ">", api.node.navigate.sibling.next, opts("Next Sibling"))
+    vim.keymap.set("n", "<", api.node.navigate.sibling.prev, opts("Previous Sibling"))
+    vim.keymap.set("n", ".", api.node.run.cmd, opts("Run Command"))
+    vim.keymap.set("n", "-", api.tree.change_root_to_parent, opts("Up"))
+    vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+    vim.keymap.set("n", "bmv", api.marks.bulk.move, opts("Move Bookmarked"))
+    vim.keymap.set("n", "B", api.tree.toggle_no_buffer_filter, opts("Toggle No Buffer"))
+    vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+    vim.keymap.set("n", "C", api.tree.toggle_git_clean_filter, opts("Toggle Git Clean"))
+    vim.keymap.set("n", "[c", api.node.navigate.git.prev, opts("Prev Git"))
+    vim.keymap.set("n", "]c", api.node.navigate.git.next, opts("Next Git"))
+    vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+    vim.keymap.set("n", "D", api.fs.trash, opts("Trash"))
+    vim.keymap.set("n", "E", api.tree.expand_all, opts("Expand All"))
+    vim.keymap.set("n", "e", api.fs.rename_basename, opts("Rename: Basename"))
+    vim.keymap.set("n", "]e", api.node.navigate.diagnostics.next, opts("Next Diagnostic"))
+    vim.keymap.set("n", "[e", api.node.navigate.diagnostics.prev, opts("Prev Diagnostic"))
+    vim.keymap.set("n", "F", api.live_filter.clear, opts("Clean Filter"))
+    vim.keymap.set("n", "f", api.live_filter.start, opts("Filter"))
+    vim.keymap.set("n", "g?", api.tree.toggle_help, opts("Help"))
+    vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+    vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+    vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Git Ignore"))
+    vim.keymap.set("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
+    vim.keymap.set("n", "K", api.node.navigate.sibling.first, opts("First Sibling"))
+    vim.keymap.set("n", "m", api.marks.toggle, opts("Toggle Bookmark"))
+    vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "O", api.node.open.no_window_picker, opts("Open: No Window Picker"))
+    vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+    vim.keymap.set("n", "P", api.node.navigate.parent, opts("Parent Directory"))
+    vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+    vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+    vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+    vim.keymap.set("n", "s", api.node.run.system, opts("Run System"))
+    vim.keymap.set("n", "S", api.tree.search_node, opts("Search"))
+    vim.keymap.set("n", "U", api.tree.toggle_custom_filter, opts("Toggle Hidden"))
+    vim.keymap.set("n", "W", api.tree.collapse_all, opts("Collapse"))
+    vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+    vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+    vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+    vim.keymap.set("n", "<2-LeftMouse>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "<2-RightMouse>", api.tree.change_root_to_node, opts("CD"))
+    -- END_DEFAULT_ON_ATTACH
+
+    --
+    -- You might tidy things by removing these along with their default mapping.
+    vim.keymap.set("n", "<C-k>", "", { buffer = bufnr })
+    vim.keymap.del("n", "<C-k>", { buffer = bufnr })
+
+    -- CUSTOM KEYBINDS
+
+    vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "<cr>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+    vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+    vim.keymap.set("n", "b", api.node.open.horizontal, opts("Open: Horizontal Split"))
+    vim.keymap.set("n", "t", api.node.open.tab, opts("Open: New Tab"))
+    vim.keymap.set("n", "gh", api.node.show_info_popup, opts("Info"))
+end
 
 function M.config()
     local status_ok, nvim_tree = pcall(require, "nvim-tree")
@@ -25,7 +104,43 @@ function M.config()
 
     local tree_cb = nvim_tree_config.nvim_tree_callback
 
-    nvim_tree.setup({
+    local function natural_cmp(left, right)
+        -- Prioritize directories over files.
+        if left.type ~= "directory" and right.type == "directory" then
+            return false
+        elseif left.type == "directory" and right.type ~= "directory" then
+            return true
+        end
+
+        left = left.name:lower()
+        right = right.name:lower()
+
+        if left == right then
+            return false
+        end
+
+        for i = 1, math.max(string.len(left), string.len(right)), 1 do
+            local l = string.sub(left, i, -1)
+            local r = string.sub(right, i, -1)
+
+            if type(tonumber(string.sub(l, 1, 1))) == "number" and type(tonumber(string.sub(r, 1, 1))) == "number" then
+                local l_number = tonumber(string.match(l, "^[0-9]+"))
+                local r_number = tonumber(string.match(r, "^[0-9]+"))
+
+                if l_number ~= r_number then
+                    return l_number < r_number
+                end
+            elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
+                return l < r
+            end
+        end
+    end
+
+    require("nvim-tree").setup({
+        sort_by = function(nodes)
+            table.sort(nodes, natural_cmp)
+        end,
+        on_attach = on_attach,
         hijack_directories = {
             enable = false,
         },
@@ -133,20 +248,33 @@ function M.config()
                     col = 1,
                 },
             },
-            mappings = {
-                custom_only = false,
-                list = {
-                    { key = { "l", "<cr>", "o" }, cb = tree_cb("edit") },
-                    { key = "h", cb = tree_cb("close_node") },
-                    { key = "v", cb = tree_cb("vsplit") },
-                    { key = "b", cb = tree_cb("split") }, -- heuristic "Below"
-                    { key = "t", cb = tree_cb("tabnew") },
-                    { key = "gh", cb = tree_cb("toggle_file_info") },
-                },
-            },
+            -- mappings = {
+            --     custom_only = false,
+            --     list = {
+            --         { key = { "l", "<cr>", "o" }, cb = tree_cb("edit") },
+            --         { key = "h", cb = tree_cb("close_node") },
+            --         { key = "v", cb = tree_cb("vsplit") },
+            --         { key = "b", cb = tree_cb("split") }, -- heuristic "Below"
+            --         { key = "t", cb = tree_cb("tabnew") },
+            --         { key = "gh", cb = tree_cb("toggle_file_info") },
+            --     },
+            -- },
             number = false,
             relativenumber = false,
         },
     })
 end
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    pattern = "NvimTree*",
+    callback = function()
+        local api = require("nvim-tree.api")
+        local view = require("nvim-tree.view")
+
+        if not view.is_visible() then
+            api.tree.open()
+        end
+    end,
+})
+
 return M
