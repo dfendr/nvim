1fd4d49c24d829e59e0a667553d8d5b532801156
D. Fender <26173007+postfen@users.noreply.github.com>
2023-08-14 14:56:03 -0600

    update api calls for nvim 0.10

--- lua/fenvim/ui/lualine.lua
+++ lua/fenvim/ui/lualine.lua
@@ -77,7 +77,7 @@ function M.config()
         function()
             local msg = "No Active Lsp"
             local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-            local clients = vim.lsp.get_active_clients()
+            local clients = vim.lsp.get_clients()
             if next(clients) == nil then
                 return msg
             end
