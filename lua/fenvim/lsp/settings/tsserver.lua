e9e519b50891768971856a11ab515ab0d85a6c32
D. Fender <26173007+postfen@users.noreply.github.com>
2023-09-05 17:23:13 -0600

    replaced tsserver with vtsls

--- lua/fenvim/lsp/settings/tsserver.lua
+++ lua/fenvim/lsp/settings/tsserver.lua
@@ -1,15 +1,18 @@
 return {
-  settings = {
-    typescript = {
-      inlayHints = {
-        includeInlayEnumMemberValueHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayVariableTypeHints = true,
-      },
-    },
-  },
+    -- detached = false,
+    -- settings = {
+    --     typescript = {
+    --         inlayHints = {
+    --             includeInlayEnumMemberValueHints = true,
+    --             includeInlayFunctionLikeReturnTypeHints = true,
+    --             includeInlayFunctionParameterTypeHints = true,
+    --             includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+    --             includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+    --             includeInlayPropertyDeclarationTypeHints = true,
+    --             includeInlayVariableTypeHints = true,
+    --         },
+    --     },
+    -- },
+    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+    cmd = { "typescript-language-server", "--stdio" },
 }
