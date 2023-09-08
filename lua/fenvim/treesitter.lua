27c58ddba208074170e611f307f032e4d3eec420
D. Fender <26173007+postfen@users.noreply.github.com>
2023-09-08 09:52:37 -0600

    pinned TS version for Dart compatibility
    Added words

--- lua/fenvim/treesitter.lua
+++ lua/fenvim/treesitter.lua
@@ -1,6 +1,7 @@
 local M = {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    commit = "f2778bd",
     dependencies = {
         "HiPhish/nvim-ts-rainbow2",
         "nvim-treesitter/playground",
