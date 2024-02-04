1fac67ce5aa155a1a956b5718ea941bd5d326b4c
D. Fender <26173007+postfen@users.noreply.github.com>
2024-02-03 20:45:46 -0700

    lsp changes, bigfile optimizations. Can now read 100k line files without lag

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,6 +2,7 @@ require("core.options")
 require("core.keymaps").setup_keymaps()
 require("core.autocmds")
 require("core.globals")
+require("core.bigfile")
 
   -- vim.cmd([[colo retrobox]])
   -- vim.cmd([[colo habamax]])
