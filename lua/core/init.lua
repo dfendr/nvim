55b4717cf5d9cd6985faec424fa0d89e6fcdce56
D. Fender <26173007+postfen@users.noreply.github.com>
2024-02-04 23:31:22 -0700

    autocmd refactor, fixed neovide

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,7 +2,6 @@ require("core.options")
 require("core.keymaps").setup_keymaps()
 require("core.autocmds")
 require("core.globals")
-require("core.bigfile")
 
   -- vim.cmd([[colo retrobox]])
   -- vim.cmd([[colo habamax]])
