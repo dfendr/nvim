89d95e03992a69fba53f57ed89772ab35a2657d7
D. Fender <26173007+postfen@users.noreply.github.com>
2024-04-11 21:47:36 -0600

    erased unused options

--- lua/fenvim/colorscheme/fenbox.lua
+++ lua/fenvim/colorscheme/fenbox.lua
@@ -1,16 +1,13 @@
 local M = {}
 
 function M.config()
-    vim.o.termguicolors = false
     vim.g.fenbox_background_color = "dark"
-
     -- Enable telescope theme
     vim.g.fenbox_telescope_theme = false
     vim.g.fenbox_transparent_mode = require("core.prefs").ui.transparent
     vim.g.fenbox_comment_style = { "italic" }
     vim.g.fenbox_function_style = { "bold" }
     vim.g.fenbox_keyword_style = "NONE"
-    vim.g.fenbox_use_original_palette = true
 
     local colorscheme = "fenbox"
     local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
--- lua/fenvim/colorscheme/init.lua
+++ lua/fenvim/colorscheme/init.lua
@@ -1,7 +1,6 @@
 return {
     {
         "dfendr/fenbox",
-        -- branch = "sandbox",
         -- dir = "~/Repos/Personal/fenbox/",
         lazy = false,
         priority = 1000, -- make sure to load this before all the other start plugins
