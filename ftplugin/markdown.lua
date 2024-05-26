5c93950f211e990a43fb547f64be4bcc849f849d
D. Fender <26173007+postfen@users.noreply.github.com>
2024-05-25 18:23:55 -0600

    added markdown to word conversion

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -4,7 +4,7 @@ vim.opt_local.textwidth = 79
 vim.opt_local.spell = true
 vim.opt_local.conceallevel = 2
 
------------------------------------------------------- [[ConvertToLatex]]
+------------------------------------------------------ [[ConvertToLatexPDF]]
 -- pandox required
 function ConvertToLatex()
     local path = vim.fn.expand("%:p")
@@ -14,6 +14,27 @@ function ConvertToLatex()
     local quoted_pdf_path = vim.fn.shellescape(base_name .. ".pdf")
 
     local command = string.format('cd "%s" && pandoc %s -o %s -V geometry:margin=1in', dir, quoted_path, quoted_pdf_path)
+
+    -- Capture the output of the pandoc command for error reporting
+    local result = vim.fn.system(command)
+
+    if vim.v.shell_error == 0 then
+        vim.notify("Conversion Success", vim.log.levels.INFO)
+    else
+        vim.notify("Conversion Failure: " .. result, vim.log.levels.ERROR)
+    end
+end
+
+------------------------------------------------------ [[ConvertToWordDoc]]
+-- pandoc required
+function ConvertToWordDoc()
+    local path = vim.fn.expand("%:p")
+    local dir = vim.fn.expand("%:p:h")
+    local base_name = vim.fn.fnamemodify(path, ":r")
+    local quoted_path = vim.fn.shellescape(path)
+    local quoted_docx_path = vim.fn.shellescape(base_name .. ".docx")
+
+    local command = string.format('cd "%s" && pandoc %s -o %s', dir, quoted_path, quoted_docx_path)
 
     -- Capture the output of the pandoc command for error reporting
     local result = vim.fn.system(command)
@@ -40,6 +61,7 @@ if status_ok then
     local mappings = {
         name = "Markdown",
         l = { "<cmd>lua ConvertToLatex()<cr>", "Convert Buffer to Latex" },
+        d = { "<cmd>lua ConvertToWordDoc()<cr>", "Convert Buffer to Word Doc" },
         i = { "<cmd>GuessIndent<cr>", "Guess Indent" },
         p = { "<cmd>MarkdownPreviewToggle<cr>", "Markdown Preview On/Off" },
         P = { "<cmd>PasteImage<cr>", "Paste Image" },
