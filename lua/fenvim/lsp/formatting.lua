633e60473c6b8d26cb5227df6e68df72f8ac16c2
D. Fender <26173007+postfen@users.noreply.github.com>
2024-10-12 14:04:14 -0600

    added sql formatter

--- lua/fenvim/lsp/formatting.lua
+++ lua/fenvim/lsp/formatting.lua
@@ -8,16 +8,32 @@ return {
         conform.formatters.stylua = { prepend_args = { "--indent-type", "Spaces" } }
         -- conform.formatters.prettier = { prepend_args = { "--tab-width", "2" } }
         conform.formatters.black = { prepend_args = { "--line-length", "79" } }
+
         conform.formatters["clang-format"] = {
             prepend_args = {
                 "--style",
                 "{BasedOnStyle: Chromium, IndentWidth: 4, ColumnLimit: 80, AlignTrailingComments: true, PointerAlignment: Right, BraceWrapping: {AfterFunction: false}}",
             },
         }
+        conform.formatters["sql-formatter"] = {
+            command = "sql-formatter",
+            args = {
+                "--language",
+                "sql",
+                "--config",
+                [[{
+          "tabWidth": 4,
+          "keywordCase": "upper",
+          "linesBetweenQueries": 4
+        }]],
+            },
+            stdin = true,
+        }
         conform.setup({
             formatters_by_ft = {
                 arduino = { "clang-format" },
                 tex = { "latexindent" },
+                sql = { "sql-formatter" },
                 bash = { "shfmt" },
                 c = { "clang-format" },
                 cpp = { "clang-format" },
