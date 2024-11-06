10bd0d63760b425321f3700cae0e27435b3dbb0f
D. Fender <26173007+postfen@users.noreply.github.com>
2024-11-06 12:56:43 -0700

    added more options to neotree

--- lua/fenvim/explorer.lua
+++ lua/fenvim/explorer.lua
@@ -28,7 +28,35 @@ return {
             close_if_last_window = true,
             enable_git_status = true,
             enable_diagnostics = true,
+            open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+            sort_case_insensitive = true, -- used when sorting files and directories in the tree
+            sort_function = nil, -- use a custom function for sorting files and directories in the tree
+            -- sort_function = function (a,b)
+            --       if a.type == b.type then
+            --           return a.path > b.path
+            --       else
+            --           return a.type > b.type
+            --       end
+            --   end , -- this sorts files and directories descendantly
             default_component_configs = {
+
+                indent = {
+                    indent_size = 2,
+                    padding = 1, -- extra padding on left hand side
+                    -- indent guides
+                    with_markers = true,
+                    indent_marker = "│",
+                    last_indent_marker = "└",
+                    highlight = "NeoTreeIndentMarker",
+                    -- expander config, needed for nesting files
+                    with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+                    expander_collapsed = "",
+                    expander_expanded = "",
+                    expander_highlight = "NeoTreeExpander",
+                },
+                container = {
+                    enable_character_fade = true,
+                },
                 icon = {
                     folder_closed = "",
                     folder_open = "",
