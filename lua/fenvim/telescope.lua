6c58c0521411854c88ccdbc01fd697183527f7f7
D. Fender <26173007+postfen@users.noreply.github.com>
2025-05-02 17:33:55 +0000

    fix: move telescope-fzf cmake build to make

--- lua/fenvim/telescope.lua
+++ lua/fenvim/telescope.lua
@@ -6,7 +6,7 @@ local M = {
         "nvim-lua/plenary.nvim",
         {
             "nvim-telescope/telescope-fzf-native.nvim",
-            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+            build = "make",
         },
         "nvim-telescope/telescope-media-files.nvim",
         "nvim-telescope/telescope-file-browser.nvim",
