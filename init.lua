6325a3ae0bbe97536ae0c264bd5d0eba3963f3da
D. Fender <26173007+postfen@users.noreply.github.com>
2025-08-12 12:52:37 -0600

    removing snacks, adding neoscroll back

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ require("lazy").setup("plugins", {
     checker = { enabled = false },
     diff = { cmd = "diffview.nvim" },
     ui = { border = require("core.prefs").ui.border_style },
-    change_detection = { enabled = true },
+    change_detection = { enabled =  false },
 })
 
 if vim.g.neovide then
